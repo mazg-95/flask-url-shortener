791e6dc0e0e082c88410e346c670b44d6bac0865
MarcoZ <mazg95@gmail.com>
2022-04-30 13:19:15 -0500

    added error message and json storage

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import json
+import os.path
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -14,6 +16,18 @@ def about():
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
+        urls = {}
+
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+
+        if request.form['code'] in urls.keys():
+            return redirect(url_for('home'))
+
+        urls[request.form['code']] = {'url': request.form['url']}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
         return render_template('your-url.html', code=request.form['code'])
     else:
-        return 'This is not valid'
+        return redirect(url_for('home'))
