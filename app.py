5aa6e801b075cb1ed4ad5518a6127ed6d23d3435
MarcoZ <mazg95@gmail.com>
2022-04-30 13:24:26 -0500

    added handling for files

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import json
 import os.path
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, flash
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = '102931239123912nienfi1i'
 
 
 @app.route('/')
@@ -23,9 +25,17 @@ def your_url():
                 urls = json.load(urls_file)
 
         if request.form['code'] in urls.keys():
+            flash('That short name has already been taken. Please select another name.')
             return redirect(url_for('home'))
 
-        urls[request.form['code']] = {'url': request.form['url']}
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url': request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save(full_name)
+            urls[request.form['code']] = {'file': full_name}
+
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
         return render_template('your-url.html', code=request.form['code'])
